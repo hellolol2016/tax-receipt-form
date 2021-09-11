41f0752a8b54a64d1f1c829f58b35246af1655f4
hellolol2016 <dennisthederp812@gmail.com>
2021-09-11 15:25:39 -0700

    added pdf and some colors

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const go = document.querySelector(".submit");
 
+const go = document.querySelector(".submit");
+const themes = ["red", "green", "blue"];
 
 function getName(){
     let name = document.querySelector("#name1").value;
@@ -39,45 +40,71 @@ function testAll(){
     createReceipt(fn, ln ,a,d, m);
 }
 const receipt = document.querySelector(".receipt-container");
+receipt.style.margin = "5px";
+receipt.style.fontFamily = "'PT Sans', sans-serif";
 function clearReceipt(){
     while (receipt.firstChild) {
         receipt.removeChild(receipt.firstChild);
     }
 }
 
+function randInt(max){
+    return Math.floor(Math.random() * max);
+}
 
+var doc = jsPDF();
 
 function createReceipt(fname, lname, amt, date, method){
     const intro = document.createElement("p");
-    intro.innerHTML = "Dear " + fname + ",<br> Thank you for your generous donation to the Laugh Out Together Foundation!"+
+    intro.innerHTML = "<hr><br>Dear " + fname + ",<br> Thank you for your generous donation to the Laugh Out Together Foundation!"+
     "<br><br>Here is a summary of your donation:";
     receipt.appendChild(intro);
 
+    color = themes[randInt(3)]
+
     let line1 = document.createElement("div");
     line1.classList.add("line");
-
+    line1.classList.add(color);
     receipt.appendChild(line1);
     const deets = document.createElement("p")
-    deets.innerHTML = "<b>Organization:</b> Laugh Out Together Foundation" + "<br>" + 
-    "<b>Donor Name:</b> " + fname + " " + lname + 
-    "<br><b>Amount:</b> " + amt + "<br>" + "<b>Date:</b> " + date + "<br>" + 
-    "<b>Method:</b> " + method + "<br>"
+    deets.innerHTML = "<br><b>Organization:</b> Laugh Out Together Foundation" + "<br>" + 
+    "<br><b>Donor Name:</b> " + fname + " " + lname + 
+    "<br><br><b>Amount:</b> " + amt + "<br>" + "<br><b>Date:</b> " + date + "<br>" + 
+    "<br><b>Method:</b> " + method + "<br>"
     deets.classList.add("details");
     receipt.appendChild(deets);
 
     let line2 = document.createElement("div");
     line2.classList.add("line");
+    line2.classList.add(color);
     receipt.appendChild(line2);
 
     const final = document.createElement("p");
-    final.innerHTML = "<br> With Love,<br>The Laugh Out Together Finance Team <br><br>" + 
+    final.innerHTML = "<br> With Love,<br>The Laugh Out Together Foundationbr><br>" + 
     "<i>Laugh Out Together is a registered 501(c)(3) non-profit organization #86-2949649. <br>" +
     "Your donation is tax deductible to the extend allowed by law.<br>No goods or services were provided "+
-    "by Laugh Out Together in return for this contribution.</i>"
+    "by Laugh Out Together in return for this contribution.</i><br><hr>"
     
     receipt.appendChild(final)
 }
 
+function printDiv(divId,
+    title) {
+  
+    let mywindow = window.open('', 'PRINT', 'height=650,width=900,top=100,left=150');
+  
+    mywindow.document.write(`<html><head><title></title>`);
+    mywindow.document.write('</head><body >');
+    mywindow.document.write(document.querySelector("."+divId).innerHTML);
+    mywindow.document.write('</body></html>');
+  
+    mywindow.document.close(); // necessary for IE >= 10
+    mywindow.focus(); // necessary for IE >= 10*/
+  
+    mywindow.print();
+    mywindow.close();
 
+    return true;
+  }
 
 go.addEventListener("click", testAll)
